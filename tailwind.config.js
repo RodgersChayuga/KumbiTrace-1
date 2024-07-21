4ec9a858105c250f633fd82ded03464ae323f990
Rodgers Chayuga <chayugarodgers@gmail.com>
2024-07-22 02:28:33 +0300

    Setup Tailwindcss for styling.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./kumbitraceweb/templates/kumbitraceweb/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
